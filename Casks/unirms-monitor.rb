30a5f0f9819d94360cc8661179bdbb07e1f7ce49
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-10-05 20:35:51 +0530

    Fix method ordering to execution order
    - Order uninstall methods by execution precedence: quit, launchctl, script, delete
    - Order zap methods by execution precedence: script, trash
    - This should match CI style expectations

--- Casks/unirms-monitor.rb
+++ Casks/unirms-monitor.rb
@@ -9,20 +9,20 @@ cask "unirms-monitor" do
 
   app "UnirmsMonitor.app"
 
-  uninstall delete:     [
-              "/Applications/UnirmsMonitor.app",
-              "/Library/LaunchDaemons/com.unirms.activitymonitor.plist",
-              "/Library/LaunchDaemons/com.unirms.activitymonitor.watchdog.plist",
-            ],
+  uninstall quit:       "com.unirms.activitymonitor",
             launchctl:  [
               "com.unirms.activitymonitor",
               "com.unirms.activitymonitor.watchdog",
             ],
-            quit:       "com.unirms.activitymonitor",
             script:     {
               executable: "/bin/bash",
               args:       ["-c", "rm -f ~/Library/Logs/unirms-activity-monitor*.log"],
-            }
+            },
+            delete:     [
+              "/Applications/UnirmsMonitor.app",
+              "/Library/LaunchDaemons/com.unirms.activitymonitor.plist",
+              "/Library/LaunchDaemons/com.unirms.activitymonitor.watchdog.plist",
+            ]
 
   zap script: {
         executable: "/bin/bash",
