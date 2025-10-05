b480eebcba87fe92df667fcf7396c8f8c74ce224
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-10-05 20:49:11 +0530

    update

--- Casks/unirms-monitor.rb
+++ Casks/unirms-monitor.rb
@@ -9,13 +9,14 @@ cask "unirms-monitor" do
 
   app "UnirmsMonitor.app"
 
-  uninstall launchctl:  ["com.unirms.activitymonitor","com.unirms.activitymonitor.watchdog"],
-            quit:       "com.unirms.activitymonitor",
-            script:     {
+  uninstall launchctl: ["com.unirms.activitymonitor", "com.unirms.activitymonitor.watchdog"],
+            quit:      "com.unirms.activitymonitor",
+            script:    {
               executable: "/bin/bash",
               args:       ["-c", "rm -f ~/Library/Logs/unirms-activity-monitor*.log"],
             },
-            delete:     ["/Applications/UnirmsMonitor.app","/Library/LaunchDaemons/com.unirms.activitymonitor.plist","/Library/LaunchDaemons/com.unirms.activitymonitor.watchdog.plist"]
+            delete:    ["/Applications/UnirmsMonitor.app", "/Library/LaunchDaemons/com.unirms.activitymonitor.plist",
+                        "/Library/LaunchDaemons/com.unirms.activitymonitor.watchdog.plist"]
 
   zap script: {
         executable: "/bin/bash",
