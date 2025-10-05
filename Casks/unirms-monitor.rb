68242985ec8ea470dbfca5eacda2d92df16b2547
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-10-05 19:40:37 +0530

    Update unirms-monitor.rb

--- Casks/unirms-monitor.rb
+++ Casks/unirms-monitor.rb
@@ -2,7 +2,7 @@ cask "unirms-monitor" do
   version "1.0.1"
   sha256 "644c6f72af6fe03d7059e49d9135153ae1fcc2be27c47b7b149d06ebd4bab138" # Will be updated when DMG is uploaded
 
-  url "https://github.com/adhiman2409/unirms-monitor/releases/download/v#{version}/UnirmsMonitor-#{version}.dmg"
+  url "https://github.com/adhiman2409/unirms-monitor/releases/tag/#{version}/UnirmsMonitor-#{version}.dmg"
   name "UNIRMS Activity Monitor"
   desc "Professional activity monitoring solution for UNIRMS"
   homepage "https://github.com/adhiman2409/unirms-monitor"
